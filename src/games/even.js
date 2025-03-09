8a7b12de7e50149eac98a52530c9f0fe7e02df16
Oksana1992-hash <nox23061992@gmail.com>
2025-03-09 20:42:57 +0300

    renamed the function name

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,11 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const checkIfEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const generateQuestionAnswer = () => {
   const randomNumber = getRandomNumber();
-  const expectedAnswer = checkIfEven(randomNumber) ? 'yes' : 'no';
+  const expectedAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [randomNumber, expectedAnswer];
 };
 
--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const checkIfPrime = (number) => {
+const isPrime = (number) => {
   const upperBorder = number / 2;
   if (number < 2) return false;
   for (let i = 2; i < upperBorder; i += 1) {
@@ -14,7 +14,7 @@ const checkIfPrime = (number) => {
 
 const generateQuestionAnswer = () => {
   const randomNumber = getRandomNumber();
-  const expectedAnswer = checkIfPrime(randomNumber) ? 'yes' : 'no';
+  const expectedAnswer = isPrime(randomNumber) ? 'yes' : 'no';
   return [randomNumber, expectedAnswer];
 };
 
